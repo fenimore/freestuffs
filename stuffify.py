20f9658836f90e6125c71c1d434651030efdf334
ludos <exorable.ludos@gmail.com>
2015-04-14 09:26:18 -0400

    colorize the map

--- stuffify.py
+++ stuffify.py
@@ -17,14 +17,17 @@ def get_coordinates(location):
     coord = [lat, lon]
     return coord
     
-#TODO: method for sorting stuff
-#def sort_stuff(stuff):
-    # use regex, right?
-    #does stuff have electronics?
-    #return blue
-    #does stuff have furniture
-    #return red
-    #yello
+#Returns the color for map rendering
+def sort_stuff(stuff):
+    furniture_pattern = "(wood|shelf|table|chair|scrap)"
+    electronics_pattern = "(TV|tv|sony|Écran|speakers)"
+
+    if re.match(furniture_pattern, stuff, re.I):
+        return "#FF0000" #red
+    if re.match(electronics_pattern, stuff, re.I):
+        return "#3186cc" #blue
+    else:
+        return "#FFFF00" #yellow
 
 def post_map(freestuffs):
 	map_osm = folium.Map([45.5088, -73.5878], zoom_start=13) #Montreal
@@ -36,24 +39,27 @@ def post_map(freestuffs):
 			#poster didn't specify where in Montreal
 		thing = freestuff.thing
 		url = freestuff.url
-		image = "imagessss" #plug image url then src it in an img tag
+		image = "imagessss" #"<img src="freestuff.image "/>"
+		#plug image url then src it in an img tag
 		# name is the map posting add img src tag
 		name = image + "<br><h3>" + thing + "</h3><h4>" + place + "</h4><a href=" + url + " target='_blank'>view ad</a>"
 		try:
-			coordinates = get_coordinates(freestuff.location) 
-			#get coords returns a list (lat = 0 lon = 1)
+			coordinates = get_coordinates(freestuff.location)#get coords returns a list (lat = 0 lon = 1)
 			lat = coordinates[0]
 			lon = coordinates[1]
-		except:
+		except:	#put it on the mountain if it's "somewhere"
 			lat = 45.5088
 			lon = -73.5878
-			#put it on the mountain if it's "somewhere"
-		print(thing, place)
+
+		#print(thing, place)
 		#TODO: Change color according to regex 
 		#		searches of furniture/electronics/etc
 		#		link to image src.... ooooo that'd be slick
 		# color = sort_stuff(thing)
+		color = sort_stuff(thing)
+		
 		map_osm.circle_marker(location=[lat, lon], radius=300,
-                  popup=name, line_color='#3186cc',
-                  fill_color='#3186cc', fill_opacity=0.2)
+                  popup=name, line_color=color,
+                  fill_color=color, fill_opacity=0.2)
+		
 	map_osm.create_map(path='findit.html') #open this 
