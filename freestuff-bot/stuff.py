234f48c76247346d09ff2ea1c1844d721a1e3692
ludos <exorable.ludos@gmail.com>
2015-05-25 12:39:32 -0400

    starting to see the package form

--- freestuff-bot/stuff.py
+++ freestuff-bot/stuff.py
@@ -19,23 +19,19 @@ class Stuff(object):
     #the stringifing printer.... Python is so pretty
     def __str__(self):
         return "what: %s \n where: %s \n link: %s \n image: %s" % (self.thing, self.location, self.url, self.image)
-        
-def get_freestuffs():
-    """Set Up"""
-    place = setup_place() # user place
-    soup = setup_page(place) # soup, needs the user place for request
-    """Construction"""
-    locs = get_locations(place, soup) # locations, needs user place for fine tuning
-    urls = get_urls(soup) # urls of stuff
-    things = get_things(soup) # things of stuff
-    images = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] # get_images(soup) # WILL THIS EVERY WORK?
 
-    """Constructor Combobulator"""
-    freestuffs = [Stuff(things[x], urls[x], locs[x], images[x]) for x in range(0,20)] 
-    return freestuffs
+place = stuffify.setup_place() # user place
+soup = stuffify.setup_page(place) # soup, needs the user place for request
+"""Construction"""
+locs = stuffify.get_locations(place, soup) # locations, needs user place for fine tuning
+urls = stuffify.get_urls(soup) # urls of stuff
+things = stuffify.get_things(soup) # things of stuff
+images = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] # stuffify.get_images(soup) # WILL THIS EVERY WORK?
 
+"""Constructor Combobulator"""
+freestuffs = [Stuff(things[x], urls[x], locs[x], images[x]) for x in range(0,20)] 
 
-
+print("The most recent stuff is:\n\n", freestuffs[0])
 
 
 
